797360646b54345306a66b935163640c5e213a8c
Jason Masri <62311598+A90Jason@users.noreply.github.com>
2021-11-23 12:29:04 -0500

    initial

---
+++ backend/api.php
@@ -0,0 +1,13 @@
+<?php 
+session_start();
+require "connection.php";
+$email = "";
+$name = "";
+
+if(isset($_POST['print'])) 
+{
+    $insert_data = "INSERT INTO users(name, email, password, admin)
+                        values('name', 'email@server.com', 'pass', '0')";
+    mysqli_query($con, $insert_data);
+}
+?>
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php require_once "./backend/api.php"; ?>
+<html lang="en">
+<head>
+    
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>LOGIN PAGE</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <body>
+    <form method="POST" autocomplete="off">
+
+        <button type="submit" name="print" class="btn btn-primary">Add as Professor</button>
+    </form>
+    </body>
+</html>
