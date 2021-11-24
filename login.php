22484c1617e8d606213110b66ef827859d61674c
Dpk417182 <derrickkeough@yahoo.com>
2021-11-23 23:10:17 -0500

    started login page html & set up mainpage route

--- login.php
+++ login.php
@@ -1,4 +1,11 @@
 <?php
+$email = $password = "";
+$email_err = $password_err = "";
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    header("location: mainPage.php");
+}
 ?>
 <html>
 
@@ -12,13 +19,15 @@
 
 <body>
     <div class="main">
-        <p class="sign" align="center">Forgot Password</p>
-        <form class="form1">
-            <input class="un" type="text" name="email" align="center" placeholder="Email">
-            <button class="passwordButton" align="center">Reset Password</button>
-            <p class="forgot" align="center"><a href="./index.php"">Back To Login Page</p> 
+        <p class="sign" align="center">Sign in</p>
+        <form class="form1" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <input class="un " type="text" name="email" align="center" placeholder="Email" <?php echo (!empty($email_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $email; ?>">
+            <span class="invalid-feedback"><?php echo $email_err; ?></span>
+            <input class="pass" type="password" name="password" align="center" placeholder="Password" <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $password; ?>">
+            <span class="invalid-feedback"><?php echo $password_err; ?></span>
+            <button class="submit" align="center" value="Submit">Sign in</button>
     </div>
-     
+
 </body>
 
 </html>
