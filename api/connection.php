d000b2bc0ea76b186386760dc69c7d6523d9451f
Jason Masri <62311598+A90Jason@users.noreply.github.com>
2021-11-23 22:05:23 -0500

    lots of work
    login page/forgotpassword page/frontend

--- api/connection.php
+++ api/connection.php
@@ -1,3 +1,8 @@
 <?php 
 $con = mysqli_connect('localhost', 'root', '', 'database');
+
+if($con === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+
 ?>
---
+++ forgotPassword.php
@@ -0,0 +1,23 @@
+<?php require_once "api/api.php"; ?>
+<html>
+
+<head>
+  <link rel="stylesheet" href="css/style.css">
+  <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+  <title>Sign in</title>
+</head>
+
+<body>
+  <div class="main">
+    <p class="sign" align="center">Forgot Passwsord</p>
+    <form class="form1">
+      <input class="un" type="text" name="email" align="center" placeholder="Email">
+      <button class="passwordButton" align="center">Reset Password</button>
+      <p class="forgot" align="center"><a href="./index.php"">Back To Login Page</p> 
+    </div>
+     
+</body>
+
+</html>
