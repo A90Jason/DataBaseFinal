4021af633cb1876939cd2e0b7aeb8470ecfbd493
Dpk417182 <derrickkeough@yahoo.com>
2021-11-24 18:23:32 -0500

    formatting

--- login.php
+++ login.php
@@ -21,9 +21,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <div class="login">
         <p class="sign" align="center">Sign in</p>
         <form class="form1" action='mainPage.php' metohd="post">
-        <input class="un " type="text" name="email" align="center" placeholder="Email">
-        <input class="pass" type="password" name="password" align="center" placeholder="Password">
-        <button class="submit" align="center" value="Submit">Sign in</button>
+            <input class="un " type="text" name="email" align="center" placeholder="Email">
+            <input class="pass" type="password" name="password" align="center" placeholder="Password">
+            <button class="submit" align="center" value="Submit">Sign in</button>
     </div>
 
 </body>
