09fb5f97835ab82ff65a1493490cac5c0333fa7c
Jason Masri <62311598+A90Jason@users.noreply.github.com>
2021-11-25 18:18:26 -0500

    updates pages add new pages
    adds login features adds new pages for buttons on the main page

--- login.php
+++ login.php
@@ -1,4 +1,31 @@
 <?php
+require_once "api/api.php";
+$errors = array();
+
+if (isset($_POST['login_user'])) {
+    $email = mysqli_real_escape_string($con, $_POST['email']);
+    $password = mysqli_real_escape_string($con, $_POST['password']);
+  
+    if (empty($email)) {
+        array_push($errors, "email is required");
+    }
+    if (empty($password)) {
+        array_push($errors, "Password is required");
+    }
+  
+    if (count($errors) == 0) {
+        $password = md5($password);
+        $query = "SELECT * FROM users WHERE email='$email' AND password='$password'";
+        $results = mysqli_query($con, $query);
+        if (mysqli_num_rows($results) == 1) {
+          $_SESSION['email'] = $email;
+          $_SESSION['success'] = "You are now logged in";
+          header('location: index.php');
+        }else {
+            array_push($errors, "Wrong email/password combination");
+        }
+    }
+  }
 ?>
 <html>
 
@@ -13,17 +40,10 @@
 <body>
     <div class="login">
         <p class="sign" align="center">Sign in</p>
-<<<<<<< Updated upstream
-        <form class="form1" action='mainPage.php' metohd="post">
+        <form class="form1" action='mainPage.php' method="post">
             <input class="un " type="text" name="email" align="center" placeholder="Email">
             <input class="pass" type="password" name="password" align="center" placeholder="Password">
-            <button class="submit" align="center" value="Submit">Sign in</button>
-=======
-        <form class="form1" action='mainPage.php' method="post">
-        <input class="un " type="text" name="email" align="center" placeholder="Email">
-        <input class="pass" type="password" name="password" align="center" placeholder="Password">
-        <button class="submit" align="center" value="Submit">Sign in</button>
->>>>>>> Stashed changes
+            <button class="submit" align="center" name="login_user">Sign in</button>
     </div>
 
 </body>
--- mainPage.php
+++ mainPage.php
@@ -1,4 +1,15 @@
 <?php
+require_once "api/api.php";
+
+if (!isset($_SESSION['email'])) {
+    $_SESSION['msg'] = "You must log in first";
+    header('location: login.php');
+}
+if (isset($_GET['logout'])) {
+    session_destroy();
+    unset($_SESSION['email']);
+    header("location: login.php");
+}
 ?>
 <html>
 
@@ -12,20 +23,24 @@
 
 <body>
 <div class="mainBox">
-        <p class="sign" align="center">Welcome (Placeholder for user)</p>
+        <p class="sign" align="center">Welcome Professor</p>
         <p class="sign" align="center">Please choose the following options...</p>
-        <form class="form1" action='newBookRequestForm.php' metohd="post">
-        
-        
-        
+        <form class="form1" action='newBookRequestForm.php' method="post">
             <button class="mainPageButtons" align="center">New book request form</button>
             <p>  </p>
+        </form>
+        <form class="form1" action='viewExistingRequestForm.php' method="post">
             <button class="mainPageButtons" align="center">View existing request form</button> 
             <p>  </p>
+        </form>
+        <form class="form1" action='addBookRequestForm.php' method="post">
             <button class="mainPageButtons" align="center" >Add book request form</button> 
             <p>  </p>
+        </form>
+        <form class="form1" action='deleteBookRequest.php' method="post">
             <button class="mainPageButtons"align="center">Delete book request</button> 
             </div> 
+        </form>
 </body>
 
 </html>
