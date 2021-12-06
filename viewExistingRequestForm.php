3cb73c9637ee47446a8e3849257261fba44edc03
avgalbo <avgalbo@gmail.com>
2021-12-05 19:15:33 -0500

    updated viewExistingRequestForm Page
    I added the feature to view the list of books from the database on the page

--- viewExistingRequestForm.php
+++ viewExistingRequestForm.php
@@ -1,5 +1,3 @@
-<?php
-?>
 <html>
 
 <head>
@@ -18,9 +16,61 @@
             <button class="submit1" value="Submit">Add Book(s)</button><p><p>
             <button class="submit1" action="addBooksForm.php" value="Submit">Delete Book(s)</button><p><p>
             <button class="submit1" value="Submit">Delete Request Form</button>
-        </form>
-        <p class="link" align="center"><a href="./mainPage.php">Back</p>
-    </div>
+            <p class="link" align="center"><a href="./mainPage.php">Back</p></a>
+          </form>
+      </div>
+
+        <h1 class= "table-header" align="center"> List of Books </h1>
+        <table class="content-table">
+            <thead>
+                <tr>
+                    <th>Book Title</th>
+                    <th>Author Name</th>
+                    <th>Edition</th>
+                    <th>Publisher</th>
+                    <th>ISBN</th>
+                    <th>UserID</th>
+                    <th>BookId</th>
+                </tr>
+            </thead>
+            
+            <?php
+
+                require_once "connection.php";
+        
+                $sql = "SELECT * FROM books";
+                $result = $con-> query($sql);
+
+                if ($result-> num_rows)
+                {
+                
+                    while($row = mysqli_fetch_array($result))
+                    {
+                        echo "<tr>";
+                        echo "<td>" . $row['bookTitle'] . "</td>";
+                        echo "<td>" . $row['authorName'] . "</td>";
+                        echo "<td>" . $row['edition'] . "</td>";
+                        echo "<td>" . $row['publisher'] . "</td>";
+                        echo "<td>" . $row['isbn'] . "</td>";
+                        echo "<td>" . $row['userID'] . "</td>";
+                        echo "<td>" . $row['bookID'] . "</td>";
+                    }
+
+
+                    echo "</table";
+                }
+                else
+                {
+                    echo "0 results";
+                }
+
+                $con-> close();
+
+
+                ?>
+
+        
+</table>
 
 </body>
 
