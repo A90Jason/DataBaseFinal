332cf297306ffdda4dbfa7f043cf647e77417f09
Dpk417182 <derrickkeough@yahoo.com>
2021-12-05 20:14:05 -0500

    individual invitation email working

--- individualRequest.php
+++ individualRequest.php
@@ -1,5 +1,19 @@
 <?php
+require_once "connection.php";
+$to_email = "";
+$subject = "Invitation to submit book requests for this semester";
+$message = "Hello Professor,
 
+    This email is to serve as an invitation to use the book request system built for COP-4710 as a final project! Please use the included link below to access the site, or to create an account if this is your first time using the system. If you feel you have received this email in error, feel free to disregard this email.
+    
+    https://localhost/DataBaseFinal/signUp.php
+";
+$headers = "From: cop4710finalproject@gmail.com";
+
+if (isset($_POST['sendEmail'])) {
+    $to_email = mysqli_real_escape_string($con, $_POST['email']);
+    $result = mail($to_email, $subject, $message, $headers);
+}
 ?>
 <html>
 
@@ -14,6 +28,11 @@
 
 <body>
     <div class="mainBox">
+        <p class="sign" align="center">Invite Professor to create book requests</p>
+        <form class="form1" method="post">
+            <input class="un " type="text" name="email" align="center" placeholder="Professor to Email">
+            <button class="submit" align="center" name="sendEmail">Send email</button>
+        </form>
         <p class="link" align="center"><a href="./staffPage.php">Back</p>
     </div>
 </body>
