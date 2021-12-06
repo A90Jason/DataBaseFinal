79f48d66113f28e3db0a0db46b3e442b51a10c00
Dpk417182 <derrickkeough@yahoo.com>
2021-12-05 19:22:14 -0500

    set up individual request email page

---
+++ individualRequest.php
@@ -0,0 +1,21 @@
+<?php
+
+?>
+<html>
+
+<head>
+    <link rel="stylesheet" href="CSS/style.css">
+    <link rel="stylesheet" href="css/mainPageStyle.css">
+    <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+    <title>Individual Request Page</title>
+</head>
+
+<body>
+    <div class="mainBox">
+        <p class="link" align="center"><a href="./staffPage.php">Back</p>
+    </div>
+</body>
+
+</html>
